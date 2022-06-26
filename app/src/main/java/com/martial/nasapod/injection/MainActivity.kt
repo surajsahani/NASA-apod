123521e44d8fd9e34bae6f64d7ef1f53db151cd6
Your Name <you@example.com>
2022-06-26 13:23:36 +0530

    removing copy paste code

---
+++ app/src/main/java/com/martial/nasapod/injection/MainActivity.kt
@@ -0,0 +1,10 @@
+package com.martial.nasapod
+
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * @Author: surajsahani
+ * @Date: 26/06/22
+ */
+class MainActivity : AppCompatActivity() {
+}
